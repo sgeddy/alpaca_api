98dc85ffd7a26da74ac5c4296ca80bf77b6223a1
Samuel Grayson Eddy II <sgeddy@ncsu.edu>
2020-10-15 19:16:10 -0400

    Update test_poly.js

--- test_poly.js
+++ test_poly.js
@@ -2,9 +2,9 @@ const Alpaca = require('@alpacahq/alpaca-trade-api')
 const alpaca = new Alpaca()
 const fetch = require("node-fetch");
 
-//  https://api.polygon.io/v2/reference/financials/AAPL?apiKey=AKX5PHEQDKKD1QQMEFWP
+//  https://api.polygon.io/v2/reference/financials/AAPL?apiKey=YOUR API KEY
 
-let theUrl = "https://api.polygon.io/v2/reference/financials/AAPL?apiKey=AKX5PHEQDKKD1QQMEFWP"
+let theUrl = "https://api.polygon.io/v2/reference/financials/AAPL?apiKey=PUT YOUR API KEY HERE"
 
 fetch(theUrl).then(function(response) {
   return response.json();
